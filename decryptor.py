61ac785fc5de9b3b02986c7a820012a03b08bf1c
BuhuanJin <sdsdjsasdj@outlook.com>
2021-03-25 12:15:56 +0800

    Update decryptor.py

--- decryptor.py
+++ decryptor.py
@@ -4,19 +4,23 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
  
 private_key = "你生成的私钥"
- 
+encrypted = "复制加密的内容过来"
+
+# 应用秘钥对RSA类进行实例化
 rsakey = RSA.importKey(private_key)
 rsakey = PKCS1_OAEP.new(rsakey)
 
+# 对编码文件Base64解码
 chunk_size = 256
 offset     = 0
 decrypted  = ""
 encrypted  = base64.b64decode(encrypted)
 
+# 以256个字节为一块来解密数据
 while offset < len(encrypted):
   decrypted += rsakey.decrypt(encrypted[offset:offset+chunk_size]) 
   offset    += chunk_size
 
-# 解压负载
+# 将这些负载解压，因为之前是压缩过之后再加密处理的
 plaintext = zlib.decompress(decrypted)
 print plaintext
